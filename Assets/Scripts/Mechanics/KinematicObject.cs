290c747f1abe4e77b1f4c391e82797f4b6be3f53
Thomas Dufresne <thomas.dufresne@woozworld.com>
2023-03-20 17:50:00 -0400

    Added playerController and kinematicObject scripts (characterController)

---
+++ Assets/Scripts/Mechanics/KinematicObject.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    public class KinematicObject : MonoBehaviour
+    {
+        public float minGroundNormalY = .65f;
+        public float gravityModifier = 1f;
+        public Vector2 velocity;
+        public bool IsGrounded { get; private set; }
+        protected Vector2 targetVelocity;
+        protected Vector2 groundNormal;
+        protected Rigidbody2D body;
+        protected ContactFilter2D contactFilter;
+        protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+        protected const float minMoveDistance = 0.001f;
+        protected const float shellRadius = 0.01f;
+
+        public void Bounce(float value)
+        {
+            velocity.y = value;
+        }
+
+        public void Bounce(Vector2 dir)
+        {
+            velocity.y = dir.y;
+            velocity.x = dir.x;
+        }
+
+        public void Teleport(Vector3 position)
+        {
+            body.position = position;
+            velocity *= 0;
+            body.velocity *= 0;
+        }
+
+        protected virtual void OnEnable()
+        {
+            body = GetComponent<Rigidbody2D>();
+            body.isKinematic = true;
+        }
+
+        protected virtual void OnDisable()
+        {
+            body.isKinematic = false;
+        }
+
+        protected virtual void Start()
+        {
+            contactFilter.useTriggers = false;
+            contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+            contactFilter.useLayerMask = true;
+        }
+
+        protected virtual void Update()
+        {
+            targetVelocity = Vector2.zero;
+            ComputeVelocity();
+        }
+
+        protected virtual void ComputeVelocity()
+        {
+
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            if (velocity.y < 0)
+                velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+            else
+                velocity += Physics2D.gravity * Time.deltaTime;
+
+            velocity.x = targetVelocity.x;
+
+            IsGrounded = false;
+
+            var deltaPosition = velocity * Time.deltaTime;
+
+            var moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+            var move = moveAlongGround * deltaPosition.x;
+
+            PerformMovement(move, false);
+
+            move = Vector2.up * deltaPosition.y;
+
+            PerformMovement(move, true);
+
+        }
+
+        void PerformMovement(Vector2 move, bool yMovement)
+        {
+            var distance = move.magnitude;
+
+            if (distance > minMoveDistance)
+            {
+                var count = body.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+                for (var i = 0; i < count; i++)
+                {
+                    var currentNormal = hitBuffer[i].normal;
+
+                    if (currentNormal.y > minGroundNormalY)
+                    {
+                        IsGrounded = true;
+
+                        if (yMovement)
+                        {
+                            groundNormal = currentNormal;
+                            currentNormal.x = 0;
+                        }
+                    }
+                    if (IsGrounded)
+                    {
+
+                        var projection = Vector2.Dot(velocity, currentNormal);
+                        if (projection < 0)
+                        {
+
+                            velocity = velocity - projection * currentNormal;
+                        }
+                    }
+                    else
+                    {
+
+                        velocity.x *= 0;
+                        velocity.y = Mathf.Min(velocity.y, 0);
+                    }
+                    
+                    var modifiedDistance = hitBuffer[i].distance - shellRadius;
+                    distance = modifiedDistance < distance ? modifiedDistance : distance;
+                }
+            }
+            body.position = body.position + move.normalized * distance;
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/PlayerController.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using Platformer.Model;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    public class PlayerController : KinematicObject
+    {
+        /// <summary>
+        /// Max horizontal speed of the player.
+        /// </summary>
+        public float maxSpeed = 7f;
+
+        /// <summary>
+        /// Initial jump velocity at the start of a jump.
+        /// </summary>
+        public float jumpTakeOffSpeed = 7;
+
+        public JumpState jumpState = JumpState.Grounded;
+        private bool stopJump;
+
+        // private Health _health;
+        private AudioSource _audioSource;
+        private Collider2D _collider2d;
+        private SpriteRenderer _spriteRenderer;
+        private Animator _animator;
+        public bool controlEnabled = true;
+        Vector2 move;
+        bool jump;
+        public PlatformerModel model;
+
+        void Awake()
+        {
+            // _health = GetComponent<Health>();
+            _audioSource = GetComponent<AudioSource>();
+            _collider2d = GetComponent<Collider2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
+        }
+
+        protected override void Update()
+        {
+            if (controlEnabled)
+            {
+                move.x = Input.GetAxis("Horizontal");
+                if (jumpState == JumpState.Grounded && Input.GetButtonDown("Jump"))
+                    jumpState = JumpState.PrepareToJump;
+                else if (Input.GetButtonUp("Jump"))
+                {
+                    stopJump = true;
+                }
+            }
+            else
+            {
+                move.x = 0;
+            }
+            UpdateJumpState();
+            base.Update();
+        }
+
+        void UpdateJumpState()
+        {
+            jump = false;
+            switch (jumpState)
+            {
+                case JumpState.PrepareToJump:
+                    jumpState = JumpState.Jumping;
+                    jump = true;
+                    stopJump = false;
+                    break;
+                case JumpState.Jumping:
+                    if (!IsGrounded)
+                    {
+                        jumpState = JumpState.InFlight;
+                    }
+                    break;
+                case JumpState.InFlight:
+                    if (IsGrounded)
+                    {
+                        jumpState = JumpState.Landed;
+                    }
+                    break;
+                case JumpState.Landed:
+                    jumpState = JumpState.Grounded;
+                    break;
+            }
+        }
+
+        protected override void ComputeVelocity()
+        {
+            if (jump && IsGrounded)
+            {
+                velocity.y = jumpTakeOffSpeed * model.jumpModifier;
+                jump = false;
+            }
+            else if (stopJump)
+            {
+                stopJump = false;
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * model.jumpDeceleration;
+                }
+            }
+
+            if (move.x > 0.01f)
+                _spriteRenderer.flipX = false;
+            else if (move.x < -0.01f)
+                _spriteRenderer.flipX = true;
+
+            _animator.SetBool("grounded", IsGrounded);
+            _animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        public enum JumpState
+        {
+            Grounded,
+            PrepareToJump,
+            Jumping,
+            InFlight,
+            Landed
+        }
+    }
+}
